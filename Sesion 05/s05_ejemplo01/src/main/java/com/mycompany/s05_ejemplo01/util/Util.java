557de8c10ccada0b3194a5447e90f66a218ebe9c
geanc22 <geanccarrionh@gmail.com>
2022-12-12 11:35:03 -0500

    sesion 5

---
+++ Sesion 05/s05_ejemplo01/src/main/java/com/mycompany/s05_ejemplo01/util/Util.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.s05_ejemplo01.util;
+
+/**
+ *
+ * @author Gean C
+ */
+public class Util {
+     public static  final int CATEGORIAA = 0;
+    public static  final int CATEGORIAB = 1;
+    public static  final int CATEGORIAC = 2;
+    public static  final int CATEGORIAD = 3;
+    
+    public static  final int SUELDOCATEGORIAA = 3500;
+    public static  final int SUELDOCATEGORIAB = 3000;
+    public static  final int SUELDOCATEGORIAC = 2500;
+    public static  final int SUELDOCATEGORIAD = 2000;
+    
+}
