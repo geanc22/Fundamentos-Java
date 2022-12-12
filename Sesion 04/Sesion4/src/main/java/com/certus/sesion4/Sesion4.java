f0dbffe39f036cdae5cfa433cb727bd607836875
geanc22 <geanccarrionh@gmail.com>
2022-12-11 22:17:34 -0500

    segunda subida

---
+++ Sesion 04/Sesion4/src/main/java/com/certus/sesion4/Sesion4.java
@@ -0,0 +1,19 @@
+
+package com.certus.sesion4;
+
+import com.certus.sesion4.ui.EscuelaLogin;
+
+
+/**
+ *
+ * @author ruben
+ */
+public class Sesion4 {
+
+    public static void main(String[] args) {
+        EscuelaLogin main=new EscuelaLogin();
+       
+        main.setVisible(true);
+        
+    }
+}
